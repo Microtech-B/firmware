17d87322a4a8d2eb12cb6973cb6e6ffdc260d995
MICROTECHBEING <mtb.micro@gmail.com>
2020-11-24 20:59:14 +0700

    20201124_205547

--- test-ota/esp32_HTTPS_git_update/include/version.h
+++ test-ota/esp32_HTTPS_git_update/include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "184"
+  #define BUILD_NUMBER "193"
 #endif
 #ifndef VERSION
-  #define VERSION "0.1.184"
+  #define VERSION "0.1.193"
 #endif
 #ifndef VERSION_DATETIME
-  #define VERSION_DATETIME "20201124_152052"
+  #define VERSION_DATETIME "20201124_205547"
 #endif
